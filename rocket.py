3edbcbd1e0d9649b62b454b2c7e330bf5a7005a9
Samuel Amankwah Donkor <56901167+samadon1@users.noreply.github.com>
2020-11-26 16:26:42 +0000

    Add files via upload

---
+++ rocket.py
@@ -0,0 +1,79 @@
+import streamlit as st
+
+st.set_page_config(page_title='Rocket Lauch Delays', page_icon='https://assets.stickpng.com/images/58e911aceb97430e819064d8.png', layout='centered', initial_sidebar_state='auto')
+
+st.write("""
+# Predict Rocket Launch Delays :rocket: 
+""")
+
+st.image('https://images.unsplash.com/photo-1580551730007-11f498ebb39d?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTl8fHJvY2tldHxlbnwwfDB8MHw%3D&auto=format&fit=crop&w=500&q=60', width = 700)
+
+crew = st.selectbox('0 - Uncrewed,  1 - Crewed ', (0, 1))
+
+st.write('## Temperatrue :fire:')
+high_temp = st.slider("High temp ", 0, 100)
+st.write("High temp. is", high_temp)
+
+low_temp = st.slider("Low temp", 0, 100)
+st.write("Low temp. is", low_temp)
+
+ave_temp = st.slider("Ave temp", 0, 100)
+st.write("Ave temp. is", ave_temp)
+
+
+
+hist_high_temp = st.slider("Hist High temp", 0, 100)
+st.write("Hist High temp. is", hist_high_temp)
+
+hist_low_temp = st.slider("Hist Low temp", 0, 100)
+st.write("Hist Low temp. is", hist_low_temp)
+
+hist_ave_temp = st.slider("Hist Ave temp", 0, 100)
+st.write("Hist Ave temp. is", hist_ave_temp)
+
+
+st.write('## Precipitation :cloud:')
+precipitation = st.number_input('Percipitation at Launch Time')
+st.write("Percipitation at Launch Time is", precipitation)
+
+hist_ave_precipitation = st.number_input('Hist Ave Percipitation is')
+st.write("Hist Ave Percipitation is", hist_ave_precipitation)
+
+
+
+st.write('## Wind :cyclone:')
+
+wind_direction = st.selectbox(
+    "Wind direction",
+    ('N', 'S','E', 'W', 'NE', 'NW', 'SE', 'SW')
+)
+
+max_wind_speed = st.number_input('Max Wind Speed')
+st.write("Max Wind Speed is", max_wind_speed)
+
+visibility = st.number_input('Visibility')
+st.write("Visibility is", visibility)
+
+wind_speed = st.number_input('Wind speed at launch time')
+st.write("Wind speed at launch time is", wind_speed)
+
+
+condition = st.selectbox(
+    "Condition",
+    ('Cloudy', 'Partly cloudy', 'Fair', 'Rain', 'Thunder', 'Heavy storm')
+)
+
+
+# display = ('Cloudy', 'Partly cloudy', 'Fair', 'Rain', 'Thunder', 'Heavy storm')
+# options = list(range(len(display)))
+# value = st.selectbox("Condition", options, format_func=lambda x: display[x])
+# condition = value
+
+st.sidebar.button('FINISH')
+
+
+
+
+# Crewed or Uncrewed	High Temp	Low Temp	Ave Temp	Temp at Launch Time	Hist High Temp	Hist Low Temp	Hist Ave Temp	Percipitation at Launch Time	Hist Ave Percipitation	Wind Direction	Max Wind Speed	Visibility	Wind Speed at Launch Time	Condition
+
+#st.balloons()
